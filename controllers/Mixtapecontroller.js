9c9fa33b8ccf3378ba64a6d945c0415de779fa24
yotamba12 <yotambaram@gmail.com>
2020-04-20 11:51:15 -0700

    first upload

---
+++ controllers/Mixtapecontroller.js
@@ -0,0 +1,25 @@
+// take the requests from the web an
+
+var express = require("express");
+
+var router = express.Router();
+
+
+// require the model
+var ***** = require("../models/***.js"); //<------------------------
+
+
+
+//Create all our routes and set up logic within those routes where required.
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
