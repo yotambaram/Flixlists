62e11bd9d38115c761ac798819b0c841d428661b
yotamba12 <yotambaram@gmail.com>
2020-04-22 21:14:35 -0700

    routes update

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -1,82 +1,17 @@
-// const express = require("express");
-// const router = express.Router();
-// const db = require("../models");
-// //const bcrypt = require("bcrypt");
-
-
-
-// router.get("/", function (req, res) {
-//         res.redirect("/movies");
-// })
-
-
-// //main page, takes all user data and render
-// router.get("/movies", function (req, res) {
-//     if (req.session.user) {
-//         db.User.findAll({
-//             where: {
-//                 id: req.session.user.id //user?
-//             }
-//         }).then(dbUser => {
-//             res.render("main", dbUser)
-//         })
-//     }else {
-//         res.redirect("/login");
-//     }
-// })
-
-
-
-
-
-// // movie page (when user press on movie in the list to get more info)
-// router.get("/movies/:id", function (req, res) {
-//     if (req.session.user) {
-//         db.Movie.findOne({
-//             where: {
-//                 imdb_id: req.budy.imdb_id //user?
-//             }
-//         }).then(dbUser => {
-//             //gives back the IMDB MOVIE ID, then the front shuold get the info from api 
-//             res.json(dbUser)
-//             //res.render("detail", dbUser)
-//         })
-//     }else {
-//         res.redirect("/login");
-//     }
-// })
-
-
-// router.get("/logout", function (req, res) {
-//     req.session.destroy(function (err) {
-//         res.json('logged out');
-//     })
-// })
-
-// module.exports = router;
-
-
-
-
-
-
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
+
+
 //const bcrypt = require("bcrypt");
 router.get("/", function (req, res) {
         res.redirect("/movies");
 })
+
 //main page, takes all user data and render just if user logged in
-router.get("/movies", function (req, res) {
+router.get("/movies", function (req, res) {  
     if (req.session.user) {
-        db.User.create({
-            where: {
-                id: req.session.user.id //user?
-            }
-        }).then(dbUser => {
-            res.render("index", dbUser)
-        })
+        res.render("index", req.session.user)
     }else {
         res.redirect("/login");
     }
--- controllers/logInController.js
+++ controllers/logInController.js
@@ -1,27 +1,28 @@
-
-
-
 const express = require("express");
 const router = express.Router();
 const db = require("../models")
 const bcrypt = require("bcrypt");
+
+
 router.get("/login", function (req, res) {
     res.render("login");
 })
+
 router.post("/login", function (req, res) {
     db.User.findOne({
         where: {
             email: req.body.email
         }
     }).then(dbUser => {
-        console.log(dbUser.password)
-        console.log(req.body.password)
+        
         if (bcrypt.compareSync(req.body.password, dbUser.password)) {
             req.session.user = {
                 email: dbUser.email,
                 id: dbUser.id
             };
-            res.redirect('/movies/')
+           
+            res.send("logged in!")
+            res.redirect('/movies')
         } else {
             res.send("not logged in")  //<---------------- MAKE A HTML RES
         }
